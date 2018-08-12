45ae649f566ecb3cea353bb4eb0bc07b34614aee
Oana-Poran <oanaporan@gmail.com>
2018-08-12 11:17:27 +0300

    enemy and player classes updated.

--- js/app.js
+++ js/app.js
@@ -1,58 +1,32 @@
-// Enemies our player must avoid
-class Enemy {
-    constructor (x, y, speed) {
+// Enemies the player must avoid
+class Enemy { 
+    constructor (x, y, speed, allEnemies = []) {
         this.x = x;
         this.y = y;
         this.speed = speed;
+        this.allEnemies = allEnemies;
+    // The image/sprite for our enemies, this uses
+    // a helper provided to easily load images
         this.sprite = 'images/enemy-bug.png';
     }
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
     update(dt) {
+
+
+          // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
         
     }
+    // Draw the enemy on screen
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
 };
 
-
-class Player {
-    constructor(x, y, move) {
-        this.x = x;
-        this.y = y;
-        this.move = move;
-        this.sprite = 'images/char-horn-girl.png';
-    }
-
-}
-
-
-
-
-
-
-const Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
+const enemies = new Enemy();
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
@@ -62,6 +36,29 @@ Enemy.prototype.render = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+class Player {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.sprite = 'images/char-boy.png';
+    }
+    update() {
+
+    }
+    // Draw the player on screen
+    render() {
+            ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+
+    }
+    handleInput() {
+
+
+    }
+};
+
+const player = new Player();
 
 
 // This listens for key presses and sends the keys to your
--- js/engine.js
+++ js/engine.js
@@ -173,7 +173,7 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-horn-girl'
+        'images/char-boy.png'
     ]);
     Resources.onReady(init);
 
