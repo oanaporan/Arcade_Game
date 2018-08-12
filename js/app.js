74d2938d650730272182fda3e3428f21708b3da6
Oana-Poran <oanaporan@gmail.com>
2018-08-12 13:58:51 +0300

    Player update(dt) method updated.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ class Enemy {
         this.x = x;
         this.y = y;
         this.speed = speed;
-        // The image/sprite for our enemies, this uses a helper provided to easily load images
+        // The image/sprite for the enemies, this uses a helper provided to easily load images
         this.sprite = 'images/enemy-bug.png';
     }
     // Update the enemy's position, required method for game
@@ -28,25 +28,23 @@ let enemyLocations = [63, 146, 230].forEach(enemyLocation => {
     allEnemies.push(enemy);
 });
 
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
+// Player 
 class Player {
     constructor(x, y) {
         this.x = x;
         this.y = y;
         this.sprite = 'images/char-boy.png';
-    }
-    update() {
+    };
+    update(dt) {
+        //if player get to the top of the board reinitialize its location
+        if (this.y < 0) {
+            setTimeout(() => {
+            player.x = 202;
+            player.y = 404;
+        }, 600);
+        }
 
-    }
+    };
     // Draw the player on screen
     render() {
             ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -66,13 +64,7 @@ class Player {
         if (keyPress == 'down' && this.y < 404) {
             this.y += 83;
         };
-        //if player get to the top of the board reinitialize its location
-        if (this.y < 0) {
-            setTimeout(() => {
-            player.x = 202;
-            player.y = 404;
-        }, 600);
-        }
+        
        
     }
 };
